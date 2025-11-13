cb1ea55938b7239bca2bfc978abbf95104a50c42
Crystal2020Inno <crystal.foo@2020innovation.com>
2025-11-13 11:26:11 +0000

    Add toggle functionality for subscribe button

---
+++ button.js
@@ -0,0 +1,11 @@
+<script>
+document.addEventListener('DOMContentLoaded', () => {
+    const buttonel = document.querySelector('.buttonel');
+    const subscribe = document.querySelector('.subscribe');
+
+    buttonel.addEventListener('click', () => {
+        subscribe.classList.toggle('show');
+    });
+});
+
+</script>
